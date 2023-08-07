7e4656bc914aec8f2fc77df8586909d8dd7fb279
PioKl <kosiu342@gmail.com>
2023-08-07 17:39:17 +0200

    Add Auth

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useCallback } from "react";
 import {
   BrowserRouter as Router,
   Route,
@@ -10,32 +10,71 @@ import Users from "./user/pages/Users";
 import NewPlace from "./places/pages/NewPlace";
 import UserPlaces from "./places/pages/UserPlaces";
 import UpdatePlace from "./places/pages/UpdatePlace";
+import Auth from "./user/pages/Auth";
 import MainNavigation from "./shared/components/Navigation/MainNavigation";
+import { AuthContext } from "./shared/context/auth-context";
 
 const App = () => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  const login = useCallback(() => {
+    setIsLoggedIn(true);
+  }, []);
+
+  const logout = useCallback(() => {
+    setIsLoggedIn(false);
+  }, []);
+
+  let routes;
+
+  if (isLoggedIn) {
+    routes = (
+      <Switch>
+        {/* switch spowoduje, że zanim odpali się redirect, to sprawdzi, czy dana ścieżka, którą wpisuję jest zawarta w gdzieś w Switchu, exact i exact={true} to jest to samo*/}
+        <Route path="/" exact={true}>
+          <Users />
+        </Route>
+        <Route path="/:userId/places" exact>
+          <UserPlaces />
+        </Route>
+        <Route path="/places/new" exact={true}>
+          <NewPlace />
+        </Route>
+        <Route path="/places/:placeId">
+          <UpdatePlace />
+        </Route>
+        <Redirect to="/" />
+      </Switch>
+      /* jeśli ścieża jest jakaś inna to powrót na główną, czyli taka ścieżka, która nie istnieje */
+    );
+  } else {
+    routes = (
+      <Switch>
+        {/* switch spowoduje, że zanim odpali się redirect, to sprawdzi, czy dana ścieżka, którą wpisuję jest zawarta w gdzieś w Switchu*/}
+        <Route path="/" exact>
+          <Users />
+        </Route>
+        <Route path="/:userId/places" exact>
+          <UserPlaces />
+        </Route>
+        <Route path="/auth">
+          <Auth />
+        </Route>
+        <Redirect to="/auth" />
+      </Switch>
+      /* jeśli ścieża jest jakaś inna to powrót do auth, czyli taka ścieżka, która nie istnieje daje powrót do auth */
+    );
+  }
+
   return (
-    <Router>
-      <MainNavigation />
-      <main>
-        <Switch>
-          {/* switch spowoduje, że zanim odpali się redirect, to sprawdzi, czy dana ścieżka, którą wpisuję jest zawarta w gdzieś w Switchu*/}
-          <Route path="/" exact={true}>
-            <Users />
-          </Route>
-          <Route path="/:userId/places" exact>
-            <UserPlaces />
-          </Route>
-          <Route path="/places/new" exact={true}>
-            <NewPlace />
-          </Route>
-          <Route path="/places/:placeId">
-            <UpdatePlace />
-          </Route>
-          <Redirect to="/" />
-        </Switch>
-        {/* jeśli ścieża jest jakaś inna to powrót na główną, czyli taka ścieżka, która nie istnieje */}
-      </main>
-    </Router>
+    <AuthContext.Provider
+      value={{ isLoggedIn: isLoggedIn, login: login, logout: logout }}
+    >
+      <Router>
+        <MainNavigation />
+        <main>{routes}</main>
+      </Router>
+    </AuthContext.Provider>
   );
 };
 
