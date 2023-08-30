5218435a27dda92e7e29e1ce723ca43c7a72a4fd
PioKl <kosiu342@gmail.com>
2023-08-30 18:20:39 +0200

    Handling CORS Errors

--- src/user/pages/Auth.js
+++ src/user/pages/Auth.js
@@ -59,20 +59,17 @@ const Auth = () => {
     if (isLoginMode) {
     } else {
       try {
-        const response = await fetch(
-          "https://localhost:5000/api/users/signup",
-          {
-            method: "POST",
-            headers: {
-              "Content-Type": "application/json",
-            },
-            body: JSON.stringify({
-              name: formState.inputs.name.value,
-              email: formState.inputs.email.value,
-              password: formState.inputs.password.value,
-            }),
-          }
-        );
+        const response = await fetch("http://localhost:5000/api/users/signup", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            name: formState.inputs.name.value,
+            email: formState.inputs.email.value,
+            password: formState.inputs.password.value,
+          }),
+        });
         const responseData = await response.json();
         console.log(responseData);
       } catch (err) {
