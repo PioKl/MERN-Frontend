7d4d59125d29b1405e8ba4d459e0e05adadc21e9
PioKl <kosiu342@gmail.com>
2023-08-31 16:46:37 +0200

    Using the Http Hook to GET Users

--- src/user/pages/Users.js
+++ src/user/pages/Users.js
@@ -3,42 +3,30 @@ import React, { useEffect, useState } from "react";
 import UserList from "../components/UserList";
 import ErrorModal from "../../shared/components/UIElements/ErrorModal";
 import LoadingSpinner from "../../shared/components/UIElements/LoadingSpinner";
+import { useHttpClient } from "../../shared/hooks/http-hook";
 
 const Users = () => {
-  const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState();
+  const { isLoading, error, sendRequest, clearError } = useHttpClient();
   const [loadedUsers, setLoadedUsers] = useState();
   //w useEffect lepiej nie używać promisów, czyli async await, można ale nie zalecane, useEfffect nie oczekuje tego, wbrew jego działaniu, nie jest to rekomendowane, czyli useEffect(async () => {}) tak nie robić
   useEffect(() => {
     //dla fetch() domyślnym requestem (żądaniem) jest GET
-    const sendRequest = async () => {
-      setIsLoading(true);
-
+    const fetchUsers = async () => {
       try {
-        const response = await fetch("http://localhost:5000/api/users");
-
-        const responseData = await response.json();
-
-        if (!response.ok) {
-          throw new Error(responseData.message);
-        }
+        //domyślnie w http-hook jest GET więc nie trzeba go tu dodawać
+        const responseData = await sendRequest(
+          "http://localhost:5000/api/users"
+        );
 
         setLoadedUsers(responseData.users);
-      } catch (err) {
-        setError(err.message);
-      }
-      setIsLoading(false);
+      } catch (err) {}
     };
-    sendRequest();
-  }, []);
-
-  const errorHandler = () => {
-    setError(null);
-  };
+    fetchUsers();
+  }, [sendRequest]);
 
   return (
     <>
-      <ErrorModal error={error} onClear={errorHandler} />
+      <ErrorModal error={error} onClear={clearError} />
       {isLoading && (
         <div className="center">
           <LoadingSpinner />
