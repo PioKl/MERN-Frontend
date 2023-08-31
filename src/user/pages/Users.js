45910b48f3ddcea8d51257b5dda359ed67a0f08a
PioKl <kosiu342@gmail.com>
2023-08-31 15:54:50 +0200

    Getting Users with a GET Request

--- src/user/pages/Users.js
+++ src/user/pages/Users.js
@@ -1,18 +1,52 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 import UserList from "../components/UserList";
+import ErrorModal from "../../shared/components/UIElements/ErrorModal";
+import LoadingSpinner from "../../shared/components/UIElements/LoadingSpinner";
 
 const Users = () => {
-  const USERS = [
-    {
-      id: "u1",
-      name: "test1",
-      image:
-        "https://images.pexels.com/photos/839011/pexels-photo-839011.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
-      places: 3,
-    },
-  ]; //dummy data
-  return <UserList items={USERS} />;
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState();
+  const [loadedUsers, setLoadedUsers] = useState();
+  //w useEffect lepiej nie używać promisów, czyli async await, można ale nie zalecane, useEfffect nie oczekuje tego, wbrew jego działaniu, nie jest to rekomendowane, czyli useEffect(async () => {}) tak nie robić
+  useEffect(() => {
+    //dla fetch() domyślnym requestem (żądaniem) jest GET
+    const sendRequest = async () => {
+      setIsLoading(true);
+
+      try {
+        const response = await fetch("http://localhost:5000/api/users");
+
+        const responseData = await response.json();
+
+        if (!response.ok) {
+          throw new Error(responseData.message);
+        }
+
+        setLoadedUsers(responseData.users);
+      } catch (err) {
+        setError(err.message);
+      }
+      setIsLoading(false);
+    };
+    sendRequest();
+  }, []);
+
+  const errorHandler = () => {
+    setError(null);
+  };
+
+  return (
+    <>
+      <ErrorModal error={error} onClear={errorHandler} />
+      {isLoading && (
+        <div className="center">
+          <LoadingSpinner />
+        </div>
+      )}
+      {!isLoading && loadedUsers && <UserList items={loadedUsers} />}
+    </>
+  );
 };
 
 export default Users;
