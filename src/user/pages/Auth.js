5cf0852b2de4455e2b12a604ad6690bcb47c798a
PioKl <kosiu342@gmail.com>
2023-09-18 18:49:03 +0200

    Using & Attaching JWT (Tokens) in React

--- src/user/pages/Auth.js
+++ src/user/pages/Auth.js
@@ -82,7 +82,7 @@ const Auth = () => {
             "Content-Type": "application/json",
           }
         );
-        auth.login(responseData.user.id);
+        auth.login(responseData.userId, responseData.token);
       } catch (err) {}
     } else {
       try {
@@ -99,7 +99,7 @@ const Auth = () => {
           formData
         );
 
-        auth.login(responseData.user.id);
+        auth.login(responseData.user.Id, responseData.token);
       } catch (err) {}
     }
   };
